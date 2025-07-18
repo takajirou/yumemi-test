9a69aaee66ffa24bb99009eb588dffe0c09a3fb6
sojiro takagi <soujirou.takagi@gmail.com>
2025-07-19 03:35:26 +0900

    refactor:ファイル名を関数名と統一

--- yumemi-test/src/hooks/usePrefectures.ts
+++ yumemi-test/src/hooks/usePrefectures.ts
@@ -1,14 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import fetchPrefectures from "@/libs/prefectureAPI";
+import { useQuery } from '@tanstack/react-query';
+import fetchPrefectures from '@/libs/fetchPrefectures';
 
 const usePrefectures = () => {
-  return useQuery({
-    queryKey: ["prefectures"],
-    queryFn: fetchPrefectures,
-    staleTime: 1000 * 60 * 5,
-    retry: 1,
-  });
+    return useQuery({
+        queryKey: ['prefectures'],
+        queryFn: fetchPrefectures,
+        staleTime: 1000 * 60 * 5, // 5分間は再フェッチしない
+        retry: 1,
+    });
 };
-
 
 export default usePrefectures;
