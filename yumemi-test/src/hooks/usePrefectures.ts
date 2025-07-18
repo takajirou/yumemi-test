ea8d800fdb51c2c0f14cc79cd29076e75b9d428b
sojiro takagi <soujirou.takagi@gmail.com>
2025-07-19 04:00:12 +0900

    feat:キャッシュ保持時間を設定に追加

--- yumemi-test/src/hooks/usePrefectures.ts
+++ yumemi-test/src/hooks/usePrefectures.ts
@@ -6,6 +6,7 @@ const usePrefectures = () => {
         queryKey: ['prefectures'],
         queryFn: fetchPrefectures,
         staleTime: 1000 * 60 * 5, // 5分間は再フェッチしない
+        gcTime: 10 * 60 * 1000, // 10分間キャッシュを保持
         retry: 1,
     });
 };
