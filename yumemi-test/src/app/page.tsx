4a3f49ef4bc84c1ab9f9d847db70bc2944436c74
sojiro takagi <soujirou.takagi@gmail.com>
2025-07-20 00:08:02 +0900

    feat:チェックボックスの表示

--- yumemi-test/src/app/page.tsx
+++ yumemi-test/src/app/page.tsx
@@ -2,13 +2,15 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PrefectureCheckbox from '@/components/prefecture/PrefectureCheckbox';
+import { useState } from 'react';
 const queryClient = new QueryClient();
 
 export default function Home() {
+    const [selectedPrefCodes, setSelectedPrefCodes] = useState<number[]>([]);
+
     return (
         <QueryClientProvider client={queryClient}>
-            <div>hello</div>
-            <PrefectureCheckbox />
+            <PrefectureCheckbox onChange={setSelectedPrefCodes} />
         </QueryClientProvider>
     );
 }
--- yumemi-test/src/components/prefecture/PrefectureCheckbox.tsx
+++ yumemi-test/src/components/prefecture/PrefectureCheckbox.tsx
@@ -1,11 +1,50 @@
 import usePrefectures from '@/hooks/usePrefectures';
+import { useEffect, useState } from 'react';
 
-const PrefectureCheckbox = () => {
+interface Pref {
+    prefCode: number;
+    prefName: string;
+}
+
+interface Props {
+    onChange: (selected: number[]) => void;
+}
+
+const PrefectureCheckbox = ({ onChange }: Props) => {
     const { data, isLoading, isError, error } = usePrefectures();
+    const [selectedPref, setSelectedPref] = useState<number[]>([]);
+
+    useEffect(() => {
+        onChange(selectedPref);
+    }, [selectedPref, onChange]);
+
     if (isLoading) return <p>読み込み中...</p>;
     if (isError) return <p>エラーが発生しました: {error.message}</p>;
 
-    console.log(data);
+    // 選択した都道府県を保存
+    const handleCheck = (prefCode: number, checked: boolean) => {
+        setSelectedPref(prev =>
+            checked ? [...prev, prefCode] : prev.filter(code => code !== prefCode)
+        );
+    };
+
+    return (
+        <div>
+            {/* APIから受け取った都道府県リストを元にチェックボックスを表示 */}
+            {data.map((pref: Pref) => (
+                <div key={pref.prefCode}>
+                    <input
+                        type="checkbox"
+                        id={`pref-${pref.prefCode}`}
+                        value={pref.prefCode}
+                        checked={selectedPref.includes(pref.prefCode)}
+                        onChange={e => handleCheck(pref.prefCode, e.target.checked)}
+                    />
+                    <label htmlFor={`pref-${pref.prefCode}`}>{pref.prefName}</label>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default PrefectureCheckbox;
